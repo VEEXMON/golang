079cb1b32e797b9e1665f7f107160959b9cc381b
Veto Firmandianta <vetovenomencer@gmail.com>
2021-02-01 23:57:52 +0700

    variable
    learning variable golang

---
+++ var.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "veto"
+
+	var lastName string
+	lastName = "firmandianta"
+
+	fmt.Printf("halo %s %s!\n", firstName, lastName)
+}
