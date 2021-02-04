d44b145708576493f721b20af28b1322ae668753
Veto Firmandianta <vetovenomencer@gmail.com>
2021-02-04 18:46:45 +0700

    decimal number

---
+++ 6-decimal-number.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var decimalNumber = 3.22
+
+	fmt.Printf("decimal number: %f\n", decimalNumber) // %f --> for format data decimal number
+	fmt.Printf("decimal number: %.3f\n", decimalNumber)
+}
