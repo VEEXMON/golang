688731d5d223ee16df9675990223c7f8f594f6ca
Veto Firmandianta <vetovenomencer@gmail.com>
2021-02-04 18:46:11 +0700

    non decimal number

---
+++ 5-non-decimal-number.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var positiveNumber uint8 = 14
+	var negativeNumber = -1324352
+
+	fmt.Printf("negative number: %d\n", positiveNumber) // %d --> for format data non decimal number
+	fmt.Printf("negative number: %d\n", negativeNumber)
+}
