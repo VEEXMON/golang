235893c0bf2b0caa146a52d75400209abc30331f
Veto Firmandianta <vetovenomencer@gmail.com>
2021-02-05 05:54:27 +0700

    constans

---
+++ 7-const.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstName string = "veto"
+	fmt.Print("halo ", firstName, "!\n")
+
+	const lastName = "firmandianta"
+	fmt.Print("nice to meet you ", lastName, "!\n")
+}
